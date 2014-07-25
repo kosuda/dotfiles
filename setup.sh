2578ac144158f5a07706bf982461cc175753119c
kosuda_tatsuya <tatsuya.kosuda@gmail.com>
2014-07-25 19:02:40 +0900

    change setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
 #! /bin/bash
 
-for file in `find $HOME/dotfiles -name '.*' -maxdepth 0 |grep -v 'dotfiles/.git$' |perl -nle 'm!dotfiles/(.+)$! and print $1'`; do
+DOT_FILES=(.vimrc,.gitconfig,.tmux.conf,.xvimrc,.zshrc)
+
+if [ ! -d $HOME/.vim/bundle ]; then
+    mkdir -p ~/.vim/bundle
+    git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
+
+for file in ${DOT_FILES[@]}; do
     ln -sf $HOME/dotfiles/$file $HOME/$file
 done
+
+if [ ! -d $HOME/.config/peco ]; then
+    mkdir -p $HOME/.config/peco
+fi
+
+ln -sf $HOME/dotfiles/config/peco/config.json $HOME/.config/peco/config.json
 
